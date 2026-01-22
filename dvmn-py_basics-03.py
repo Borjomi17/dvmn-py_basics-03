9a67a0dafdf236d7b456ef1833d9958e46131670
Borjomi17 <quick.morozov@yandex.ru>
2026-01-22 14:54:52 +0400

    Add files via upload

---
+++ dvmn-py_basics-03.py
@@ -0,0 +1,40 @@
+import smtplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+login = os.getenv('login')
+password = os.getenv('password')
+mail = '''From: {tmail}
+To: {rmail}
+Subject: Приглашение!
+Content-Type: text/plain; charset="UTF-8";
+
+Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''
+website = 'https://dvmn.org/profession-ref-program/quick.morozov/0VuHT/'
+friend_name = 'Никита'
+my_name = 'Богдан'
+rmail = 'nikita@yandex.ru'
+tmail = 'bogdan@yandex.ru'
+letter = mail.replace('%website%',website).replace('%friend_name%',friend_name).replace('%my_name%',my_name).format(rmail = rmail, tmail = tmail)
+letter = letter.encode('utf-8')
+server.login(login, password)
+server.sendmail(tmail, rmail, letter)
+server.quit()
+print(letter)
